3c7c6a089daa22a6fc8d604ae5d2b805efb4e0dc
gorettileonardo-ui <goretti.leonardo@franchettisalviani.net>
2025-12-03 10:07:40 +0100

    classe test

--- src/simulatorecalcio/SimulatoreCalcio.java
+++ src/simulatorecalcio/SimulatoreCalcio.java
@@ -14,7 +14,11 @@ public class SimulatoreCalcio {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Test t;
+
+        t = new Test();
+
+        t.avvia();
     }
     
 }
