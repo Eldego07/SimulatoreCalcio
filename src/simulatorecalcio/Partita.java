9e1e6c85a9d953fa1a033a1b16aae2bf3f48935c
Eldego07 <diego.casolaro8@gmail.com>
2025-12-03 10:00:16 +0100

    Implementazione della classe Partita

--- src/simulatorecalcio/Partita.java
+++ src/simulatorecalcio/Partita.java
@@ -4,6 +4,8 @@
  */
 package simulatorecalcio;
 
+import java.util.Objects;
+
 /**
  *
  * @author casolaro.diego
@@ -24,6 +26,57 @@ public class Partita {
     
     public void evento(){
         
+    }
+
+    public Squadra getSquadraCasa() {
+        return squadraCasa;
+    }
+
+    public void setSquadraCasa(Squadra squadraCasa) {
+        this.squadraCasa = squadraCasa;
+    }
+
+    public Squadra getSquadraTrasferta() {
+        return squadraTrasferta;
+    }
+
+    public void setSquadraTrasferta(Squadra squadraTrasferta) {
+        this.squadraTrasferta = squadraTrasferta;
+    }
+
+    public String getRisultato() {
+        return risultato;
+    }
+
+    public void setRisultato(String risultato) {
+        this.risultato = risultato;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Partita other = (Partita) obj;
+        if (!Objects.equals(this.risultato, other.risultato)) {
+            return false;
+        }
+        if (!Objects.equals(this.squadraCasa, other.squadraCasa)) {
+            return false;
+        }
+        return Objects.equals(this.squadraTrasferta, other.squadraTrasferta);
+    }
+
+    @Override
+    public String toString() {
+        return "Partita{" + "squadraCasa=" + squadraCasa + ", squadraTrasferta=" + squadraTrasferta + ", risultato=" + risultato + '}';
     }
     
 }
