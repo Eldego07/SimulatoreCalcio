d6747c021ece3a7284c7cb08f69350681567946a
TaboadaTaddeo22 <taboada.taddeo@franchettisalviani.net>
2025-12-16 22:51:24 +0100

    ho aggiunto i calciatori
    sono un coglione

--- src/simulatorecalcio/MainSimulatore.java
+++ src/simulatorecalcio/MainSimulatore.java
@@ -5,6 +5,7 @@
 package simulatorecalcio;
 
 import java.util.*;
+import java.io.*;
 
 /**
  * Classe MainSimulatore
@@ -12,6 +13,71 @@ import java.util.*;
  */
 public class MainSimulatore {
     public void avvia(){
+        ArrayList<Giocatore> lista = new ArrayList<>();
+        ArrayList<Squadra> squadre = new ArrayList<>();
+        
+        System.out.println(new File("calciatoriFOFA.txt").getAbsolutePath());
+
+        try (BufferedReader br = new BufferedReader(new FileReader("calciatoriFOFA.txt"))) {
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                String[] campi = linea.split(",");
+
+                String nome = campi[0];
+                String cognome = campi[1];
+                int overall = Integer.parseInt(campi[2]);
+                String ruolo = campi[3];
+                String squadra = campi[4];
+                Giocatore g;
+                Squadra s = null;
+                
+                switch (ruolo) {
+                    case "Portiere":
+                        g = new Portiere(nome, cognome, overall, 80);
+                        break;
+                    case "Difensore":
+                        g = new Difensore(nome, cognome, overall);
+                        break;
+                    case "Centrocampista":
+                        g = new Centrocampista(nome, cognome, overall);
+                        break;
+                    case "Attaccante":
+                        g = new Attaccante(nome, cognome, overall);
+                        break;
+                    default:
+                        g = null;
+                        break;
+                }
+                
+                for (Squadra sq : squadre) {
+                    if (sq.getNome().equals(squadra)) {
+                    s = sq;
+                    break;
+                    }
+                }
+                
+                if (s == null) {
+                    s = new Squadra(squadra, squadra);
+                    squadre.add(s);
+                }
+                
+                if (g != null) {
+                    s.aggiungiGiocatore(g);
+                }
+
+                lista.add(g);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Stampa di controllo
+        for (Giocatore g : lista) {
+            System.out.println(g);
+        }
+        
         GestoreMessaggi gM = new GestoreMessaggi();
         Scanner input = new Scanner(System.in);
         
