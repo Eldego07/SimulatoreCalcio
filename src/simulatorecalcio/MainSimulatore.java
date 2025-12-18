c083fdcab3652e1ebb5ae440321d4580b99cc682
TaboadaTaddeo22 <taboada.taddeo@franchettisalviani.net>
2025-12-18 18:41:03 +0100

    ora giocano in 11 per squadra
    eldego ti odio

--- src/simulatorecalcio/MainSimulatore.java
+++ src/simulatorecalcio/MainSimulatore.java
@@ -13,7 +13,6 @@ import java.io.*;
  */
 public class MainSimulatore {
     public void avvia(){
-        ArrayList<Giocatore> lista = new ArrayList<>();
         ArrayList<Squadra> squadre = new ArrayList<>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("calciatoriFOFA.txt"))) {
@@ -31,20 +30,15 @@ public class MainSimulatore {
                 Squadra s = null;
                 
                 switch (ruolo) {
-                    case "Portiere":
-                        g = new Portiere(nome, cognome, overall, overall);
+                    case "Portiere": g = new Portiere(nome, cognome, overall, overall);
                         break;
-                    case "Difensore":
-                        g = new Difensore(nome, cognome, overall);
+                    case "Difensore": g = new Difensore(nome, cognome, overall);
                         break;
-                    case "Centrocampista":
-                        g = new Centrocampista(nome, cognome, overall);
+                    case "Centrocampista": g = new Centrocampista(nome, cognome, overall);
                         break;
-                    case "Attaccante":
-                        g = new Attaccante(nome, cognome, overall);
+                    case "Attaccante": g = new Attaccante(nome, cognome, overall);
                         break;
-                    default:
-                        g = null;
+                    default: g = null;
                         break;
                 }
                 
@@ -63,8 +57,6 @@ public class MainSimulatore {
                 if (g != null) {
                     s.aggiungiGiocatore(g);
                 }
-
-                lista.add(g);
             }
 
         } catch (IOException e) {
@@ -78,6 +70,7 @@ public class MainSimulatore {
         
         GestoreMessaggi gM = new GestoreMessaggi();
         Scanner input = new Scanner(System.in);
+        Random rnd = new Random();
         
         Squadra sCasa = null, sTrasferta = null;
         String scelta = null;
@@ -89,7 +82,7 @@ public class MainSimulatore {
             System.out.println("Inserisci la squadra in casa (Es. Bayern Munchen, FC Barcelona)");
             scelta = input.nextLine();
             for (Squadra sq : squadre) {
-                if (sq.getNome().equals(scelta)) {
+                if (sq.getNome().equalsIgnoreCase(scelta)) {
                     sCasa = sq;
                     valido = true;
                 }
@@ -103,18 +96,76 @@ public class MainSimulatore {
             System.out.println("Inserisci la squadra in trasferta (Es. Bayern Munchen, FC Barcelona)");
             scelta = input.nextLine();
             for (Squadra sq : squadre) {
-                if (sq.getNome().equals(scelta)) {
+                if (sq.getNome().equalsIgnoreCase(scelta)) {
                     sTrasferta = sq;
                     valido = true;
                 }
             }
         }
         
+        Random rand = new Random();
+        Squadra sCasaTitolare = new Squadra(sCasa.getNome(), sCasa.getStadio());
+        boolean portiereInserito = false;
+
+        while (sCasaTitolare.getGiocatori().size() < 11) {
+            int indice = rand.nextInt(sCasa.getGiocatori().size());
+            Giocatore g = sCasa.getGiocatori().get(indice);
+
+            while (sCasaTitolare.getGiocatori().contains(g)) {
+                indice = rand.nextInt(sCasa.getGiocatori().size());
+                g = sCasa.getGiocatori().get(indice);
+            }
+            
+            while (!portiereInserito) {
+                if (g instanceof Portiere portiere) {
+                    sCasaTitolare.aggiungiGiocatore(g);
+                    portiereInserito = true;
+                } else {
+                    indice = rand.nextInt(sCasa.getGiocatori().size());
+                    g = sCasa.getGiocatori().get(indice);
+                }
+            }
+
+            if (!(g instanceof Portiere portiere)) {
+                sCasaTitolare.aggiungiGiocatore(g);
+            }
+        }       
+        
+        Squadra sTrasfertaTitolare = new Squadra(sTrasferta.getNome(), sTrasferta.getStadio());
+        portiereInserito = false;
+        
+        while (sTrasfertaTitolare.getGiocatori().size() < 11) {
+            int indice = rand.nextInt(sTrasferta.getGiocatori().size());
+            Giocatore g = sTrasferta.getGiocatori().get(indice);
+
+            while (sCasaTitolare.getGiocatori().contains(g)) {
+                indice = rand.nextInt(sCasa.getGiocatori().size());
+                g = sCasa.getGiocatori().get(indice);
+            }
+            
+            while (!portiereInserito) {
+                if (g instanceof Portiere portiere) {
+                    sTrasfertaTitolare.aggiungiGiocatore(g);
+                    portiereInserito = true;
+                } else {
+                    indice = rand.nextInt(sTrasferta.getGiocatori().size());
+                    g = sTrasferta.getGiocatori().get(indice);
+                }
+            }
+
+            if (!(g instanceof Portiere portiere)) {
+                sTrasfertaTitolare.aggiungiGiocatore(g);
+            }
+        }  
 
         
         System.out.println("");
-        gM.stampaStadio(sCasa, sTrasferta);
-        Partita p = new Partita(sCasa, sTrasferta);
+        System.out.println(sCasaTitolare.toString());
+        System.out.println("");
+        System.out.println(sTrasfertaTitolare.toString());
+        System.out.println("");
+        gM.stampaStadio(sCasaTitolare, sTrasfertaTitolare);
+        Partita p = new Partita(sCasaTitolare, sTrasfertaTitolare);
         p.gioca();
         System.out.println(p);
     }
